bc6278d0d3616540e264bc9aa51eb08c02d03f7e
Ricardo Lüders <ricardo.luders@humantech.com.br>
2015-02-13 15:21:29 -0200

    adding metadata and attachments

--- tests/CRMSocialtTest.php
+++ tests/CRMSocialtTest.php
@@ -67,4 +67,38 @@ class CRMSocialTest extends PHPUnit_Framework_TestCase
 
 	}
 
+	public function testUpdateLeadMeta()
+	{
+
+		$crmsocial = new CRMSocial(CRMSOCIAL_URL);
+
+		$params = array(
+			'page'               => 'phpunit-page',
+			'email'              => 'ricardo.luders@humantech.com.br',
+			'name'               => 'PHPUnit',
+			'phone'              => '(47) 3333-3333',
+			'city_id'            => 4414,
+			'state_id'           => 'SC',
+			'message'            => 'PHPUnit test',
+			'destination'        => 'phpunit@tuper.net',
+			'campaign_matchtype' => '',
+			'campaign_keyword'   => '',
+			'metadata'           =>  array(
+				'sex'  => 'Masculino',
+				'age'  => 32,
+				'site' => 'http://www.luders.com.br'
+			),
+			'files'              => array(
+				array(
+					"filename" => "file.csv",
+					"content"  => "RW1haWxBZGRyZXNzLFVzZXJOYW1lLFBhc3N3b3JkDQp0ZXJyeWFAY29udG9zby5lZHUsdGVycnkuYWRhbXMsMTA5MTk5MA0KYW5uYkBjb250b3NvLmVkdSxhbm4uYmVlYmUsMjExMTk5MQ0KY2hyaXNjQGNvbnRvc28uZWR1LGNocmlzLmNhbm5vbiwzMjgxOTg2"
+ 				)
+			)
+		);
+
+		$result = $crmsocial->send('update_lead', $params);
+		$this->_assert($result);
+
+	}
+
 }
