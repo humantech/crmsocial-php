74161802a30a14061165bbeae310766df3835e80
Ricardo Lüders <ricardo.luders@humantech.com.br>
2015-08-24 11:29:35 -0300

    Fix

--- tests/CRMSocialtTest.php
+++ tests/CRMSocialtTest.php
@@ -40,16 +40,16 @@ class CRMSocialTest extends PHPUnit_Framework_TestCase
         $crmsocial = new CRMSocial(CRMSOCIAL_URL);
 
         $params = [
-            'page' => 'phpunit-page',
-            'email' => 'ricardo.luders@humantech.com.br',
-            'name' => 'PHPUnit',
-            'phone' => '(47) 3333-3333',
-            'city_id' => 4414,
-            'state_id' => 'SC',
-            'message' => 'PHPUnit test',
-            'destination' => 'phpunit@tuper.net',
+            'page'               => 'phpunit-page',
+            'email'              => 'ricardo.luders@humantech.com.br',
+            'name'               => 'PHPUnit',
+            'phone'              => '(47) 3333-3333',
+            'city_id'            => 4414,
+            'state_id'           => 'SC',
+            'message'            => 'PHPUnit test',
+            'destination'        => 'phpunit@tuper.net',
             'campaign_matchtype' => '',
-            'campaign_keyword' => '',
+            'campaign_keyword'   => '',
         ];
 
         $result = $crmsocial->send('update_lead', $params);
@@ -61,25 +61,25 @@ class CRMSocialTest extends PHPUnit_Framework_TestCase
         $crmsocial = new CRMSocial(CRMSOCIAL_URL);
 
         $params = [
-            'page' => 'phpunit-page',
-            'email' => 'ricardo.luders@humantech.com.br',
-            'name' => 'PHPUnit',
-            'phone' => '(47) 3333-3333',
-            'city_id' => 4414,
-            'state_id' => 'SC',
-            'message' => 'PHPUnit test',
-            'destination' => 'phpunit@tuper.net',
+            'page'               => 'phpunit-page',
+            'email'              => 'ricardo.luders@humantech.com.br',
+            'name'               => 'PHPUnit',
+            'phone'              => '(47) 3333-3333',
+            'city_id'            => 4414,
+            'state_id'           => 'SC',
+            'message'            => 'PHPUnit test',
+            'destination'        => 'phpunit@tuper.net',
             'campaign_matchtype' => '',
-            'campaign_keyword' => '',
-            'metadata' => [
-                'sex' => 'Masculino',
-                'age' => 32,
+            'campaign_keyword'   => '',
+            'metadata'           => [
+                'sex'  => 'Masculino',
+                'age'  => 32,
                 'site' => 'http://www.luders.com.br',
             ],
             'files' => [
                 [
                     'filename' => 'file.csv',
-                    'content' => 'RW1haWxBZGRyZXNzLFVzZXJOYW1lLFBhc3N3b3JkDQp0ZXJyeWFAY29udG9zby5lZHUsdGVycnkuYWRhbXMsMTA5MTk5MA0KYW5uYkBjb250b3NvLmVkdSxhbm4uYmVlYmUsMjExMTk5MQ0KY2hyaXNjQGNvbnRvc28uZWR1LGNocmlzLmNhbm5vbiwzMjgxOTg2',
+                    'content'  => 'RW1haWxBZGRyZXNzLFVzZXJOYW1lLFBhc3N3b3JkDQp0ZXJyeWFAY29udG9zby5lZHUsdGVycnkuYWRhbXMsMTA5MTk5MA0KYW5uYkBjb250b3NvLmVkdSxhbm4uYmVlYmUsMjExMTk5MQ0KY2hyaXNjQGNvbnRvc28uZWR1LGNocmlzLmNhbm5vbiwzMjgxOTg2',
                 ],
             ],
         ];
